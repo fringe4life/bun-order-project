014c752b874d715e5abfe4ca260ff0890c566930
Coinnich <>
2025-02-25 19:20:36 +1300

    added path alias to include burger img

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 //import lightningcss from "vite-plugin-lightningcss"
 // https://vite.dev/config/
-
+import path from 'node:path'
 export default defineConfig({
   plugins: [ ],
+  resolve: {
+    alias: {
+    "@": path.resolve(__dirname, "./src")
+  }
+  },
   
   build: {
     cssMinify: 'esbuild',
